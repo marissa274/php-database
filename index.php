a8afe346257a7048505baf9c56f184c270ef5ae7
Marissa belle <marissabellebindeki@gmail.com>
2025-11-04 16:24:57 -0500

    feat: add styles to index page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pokedex</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+         <link rel="stylesheet" href="./public/stylesheets/styles.css">
+         <link rel="stylesheet" href="./public/stylesheets/pages/index.css">
+
+    </head>
+    <body>
+        <header>
+            <div class ="container">
+                <nav>
+                    <div class = "logo">
+                        <a href="index.php">Pokedex</a>
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <main class="container">
+        <h1>Welcome to the Pokedex</h1>
+        <ul>
+            <li><a href="./pokemons.php">Liste des pokemons</a></li>
+            
+        </ul>
+        </main>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </body>
+</html>
